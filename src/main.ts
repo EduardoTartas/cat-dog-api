9ac7c3c824faa3043697b52069e8c849a0e10fdc
EduardoTartas <eduardos.tartas@gmail.com>
2025-05-29 21:12:27 -0400

    Adiciona lógica para exibir fotos de cães ou gatos com base na seleção do usuário

---
+++ src/main.ts
@@ -0,0 +1,41 @@
+import './assets/styles/index.css';
+import loading from './assets/images/loading.gif';
+
+const animal = document.querySelector<HTMLInputElement>('#animal')!;
+const submit = document.querySelector<HTMLButtonElement>('#submit')!;
+const photo = document.querySelector<HTMLImageElement>('#animal-photo')!;
+
+photo.src = `${loading}`;
+
+submit.addEventListener('click', async (event) => {
+  event.preventDefault();
+
+  console.log('Clicou no botão');
+  console.log(animal.value);
+  
+  if(animal.value == 'dog') {
+    photo.src = `${loading}`;
+    photo.src = `${await getDog()}`;
+  }
+  if(animal.value == 'cat') {
+    photo.src = `${loading}`;
+    photo.src = `${await getCat()}`;
+  }
+  if(animal.value !== 'dog' && animal.value !== 'cat') {
+    alert('Por favor, ecolha entre "cachorro" ou "gato"');
+    return;
+  }
+
+});
+
+async function getDog(){
+  const response = await fetch(`https://api.thedogapi.com/v1/images/search`);
+  const data = await response.json();
+  return data[0].url;
+}
+
+async function getCat(){
+  const response = await fetch(`https://api.thecatapi.com/v1/images/search`);
+  const data = await response.json();
+  return data[0].url;
+}
